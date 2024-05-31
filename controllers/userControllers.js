35d994e1f4f28d3426014ada9ac739076acd2efa
ValRaz <valerie.dres@gmail.com>
2024-05-31 14:43:59 -0600

    swagger update completed

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -5,7 +5,7 @@ const Joi = require("joi");
 const userSchema = Joi.object({
   name: Joi.string().required(),
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().required()
 });
 
 const registerUser = async (req, res, next) => {
@@ -99,13 +99,13 @@ const getUserById = async (req, res, next) => {
   }
 
   try {
-    const client = await mongodb.connect(); 
+    const client = await mongodb.connect();
     const db = client.db("yourDatabaseName");
-    const collection = db.collection("users"); 
+    const collection = db.collection("users");
 
     const user = await collection.findOne({ _id: new ObjectId(userId) });
     if (!user) {
-      return res.status(404).json({ message: "User not found" }); 
+      return res.status(404).json({ message: "User not found" });
     }
 
     client.close();
@@ -114,6 +114,67 @@ const getUserById = async (req, res, next) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: "Error retrieving user details" });
+  }
+};
+
+const updateUser = async (req, res, next) => {
+  const userId = req.params.id;
+  const { body } = req;
+
+  if (!userId) {
+    return res.status(400).json({ message: "Missing user ID" });
+  }
+
+  const updateSchema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+  });
+
+  const { error } = updateSchema.validate(body);
+
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  try {
+    const client = await mongodb.connect();
+    const db = client.db("yourDatabaseName");
+    const collection = db.collection("users");
+
+    const update = { $set: body };
+
+    await collection.updateOne({ _id: new ObjectId(userId) }, update);
+
+    client.close();
+
+    res.status(200).json({ message: "User updated successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Errorexpand_more updating user" });
+  }
+};
+
+const deleteUser = async (req, res, next) => {
+  const userId = req.params.id;
+
+  if (!userId) {
+    return res.status(400).json({ message: "Missing user ID" });
+  }
+
+  try {
+    const client = await mongodb.connect();
+    const db = client.db("yourDatabaseName");
+    const collection = db.collection("users");
+
+    await collection.deleteOne({ _id: new ObjectId(userId) });
+
+    client.close();
+
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Errorexpand_more deleting user" });
   }
 };
 
